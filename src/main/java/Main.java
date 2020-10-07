ab3c4bb0c6de8cdf4092b75d6859d9de576a3913
cecile12240 <cecilerocquet@sfr.fr>
2020-10-07 15:53:57 +0200

    premier cour java

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/** classe de tests */
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String chainenoninitialise;
+        System.out.println("Hello World");// sortie écran
+    }
+}
+
